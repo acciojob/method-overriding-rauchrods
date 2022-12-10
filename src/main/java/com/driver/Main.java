a5a45add28bd83ae58cb256068efb1c777f73bac
rauch <rauchrodrigues@gmail.com>
2022-12-10 20:45:31 +0530

    8:44 pm 10-12-2022

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,20 @@
 package com.driver;
 
 public class Main {
-  
+    class  A{
+        String meth(){
+            return "Invoking method from class A";
+        }
+    }
+    class  B extends A{
+        String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
+
+    public void main(String[] args) {
+
+        B bobj = new B();
+        bobj.meth();
+    }
 }
