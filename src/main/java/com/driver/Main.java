04f7b6561dad80de11758fd15940bfe38ddd49b3
rauch <rauchrodrigues@gmail.com>
2022-12-11 20:15:58 +0530

    bas karo

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,20 +1,25 @@
 package com.driver;
 
+
+
+
 public class Main {
-    public static class  A{
-       public String meth(){
+
+    public static class A {
+        public String meth() {
             return "Invoking method from class A";
         }
     }
-    public class  B extends A{
+
+    public static class  B extends A{
         public String meth(){
             return "Method is overridden in Extendend class B";
         }
     }
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
 
         A obj = new B();
-        obj.meth();
+        System.out.println(obj.meth());
     }
 }
